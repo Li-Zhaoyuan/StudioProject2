6f0032bb0cf4dc374b01e167d1c8a53ac63683ba
Terence Tan Ming Jie <mingjie097@gmail.com>
2016-02-16 21:46:21 +0800

    Added a OBJBoundary header and cpp

---
+++ SP2/LiZhaoyuan1/Source/OBJBoundary.cpp
@@ -0,0 +1,69 @@
+#include "OBJBoundary.h"
+
+OBJBoundary::OBJBoundary()
+{
+
+}
+
+OBJBoundary::~OBJBoundary()
+{
+
+}
+
+// Original Boundary Code
+bool OBJBoundary::AABB(float xS, float xL, float zS, float zL)
+{
+	if ((camera.position.x > xS) && (camera.position.x < xL) && (camera.position.z > zS) && (camera.position.z < zS + 2)) { camera.position.z = zS; }
+	if ((camera.position.x > xS) && (camera.position.x < xL) && (camera.position.z < zL) && (camera.position.z > zL - 2)) { camera.position.z = zL; }
+	if ((camera.position.z > zS) && (camera.position.z < zL) && (camera.position.x > xS) && (camera.position.x < xS + 2)) { camera.position.x = xS; }
+	if ((camera.position.z > zS) && (camera.position.z < zL) && (camera.position.x > xL) && (camera.position.x < xL - 2)) { camera.position.x = xL; }
+}
+
+//Was going to Implement the follow bottom codes instead
+bool OBJBoundary::boundaryCheckerX(float smallx, float largex, float smallz, float largez, Vector3 value)
+{
+	if ((value.z > smallz) && (value.z<largez) && (value.x > smallx) && (value.x<smallx + 2)){ return false; }
+	if ((value.z > smallz) && (value.z<largez) && (value.x < largex) && (value.x>largex - 2)){ return false; }
+	return true;
+}
+
+bool OBJBoundary::boundaryCheckerZ(float smallx, float largex, float smallz, float largez, Vector3 value)
+{
+	if ((value.x > smallx) && (value.x<largex) && (value.z > smallz) && (value.z<smallz + 2)){ return false; }
+	if ((value.x > smallx) && (value.x<largex) && (value.z < largez) && (value.z>largez - 2)){ return false; }
+	return true;
+}
+
+bool OBJBoundary::OBJboundaryX(Vector3 Testvalue)
+{
+	if (boundaryCheckerX(-50, 50, -105, -40, Testvalue) == false){ return false; }
+	return true;
+}
+
+bool OBJBoundary::OBJboundaryZ(Vector3 Testvalue)
+{
+	if (boundaryCheckerZ(-50, 50, -105, -40, Testvalue) == false){ return false; }
+	return true;
+}
+
+bool OBJBoundary::OBJboundaryY(Vector3 Testvalue, int XZ)
+{
+	if (Testvalue.y >= 20)
+	{
+		if (XZ == 1)
+		{
+			if (boundaryCheckerX(-89, -70, -25, 0, Testvalue) == false)
+			{
+				return false;
+			}
+		}
+		else if (XZ == 2)
+		{
+			if (boundaryCheckerZ(-89, -70, -25, 0, Testvalue) == false)
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
